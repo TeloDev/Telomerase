f6169b342dc564ec0344214dac922f07b9484d19
facetorched <kktangent@gmail.com>
2022-06-10 22:12:32 -0500

    update mods and configs and move resourcepacks

--- config/mcinstanceloader/clear_mods.bat
+++ config/mcinstanceloader/clear_mods.bat
@@ -1,5 +1,5 @@
 SET "sourcedir=..\..\mods"
-SET "keepfile=mcinstanceloader-1.0.jar"
+SET "keepfile=mcinstanceloader-1.0-dev.jar"
 
 FOR /d %%a IN ("%sourcedir%\*") DO RD /S /Q "%%a"
 FOR %%a IN ("%sourcedir%\*") DO IF /i NOT "%%~nxa"=="%keepfile%" DEL "%%a"
