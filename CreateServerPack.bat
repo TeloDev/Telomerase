7a6a9c2d2ad3173c0f4f9164ab3d26594f0f291b
facetorched <kktangent@gmail.com>
2022-07-13 02:10:08 -0500

    add files for serverpack

---
+++ CreateServerPack.bat
@@ -0,0 +1,10 @@
+jar -cfM TelomeraseServer.zip ^
+-C . armourersWorkshop ^
+-C . bansoukou ^
+-C . config ^
+-C . mods\mcinstanceloader* ^
+-C . scripts ^
+-C . server-icon.png ^
+-C . ServerStart.bat ^
+-C . ServerStartLinux.sh ^
+-C . settings.cfg
