4ca384d6660b1f255526e246a4cbbe3df3db97f4
Kevin Tan <kktangent@gmail.com>
2025-06-30 23:59:18 -0500

    update workflows and scripts

--- UpdateVersion.sh
+++ UpdateVersion.sh
@@ -1,4 +1,4 @@
 sed -i "s/version = \".*/version = \"$1\"/" pack.toml
 sed -i "s/S:windowDisplayTitle=Telomerase .*/S:windowDisplayTitle=Telomerase $1/" ./config/itlt.cfg
 sed -i "s/S:currentPackVersion=Telomerase .*/S:currentPackVersion=Telomerase $1/" ./config/LaunchGui.cfg
-echo "Telomerase $1" > ./resources/telomerase/version.txt
+echo "Telomerase $1" > ./config/txloader/forceload/telomerase/version.txt
